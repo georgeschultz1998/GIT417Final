0c73e876ddc1158eebd7027ab526a89cb055c7a6
Anthony Schultz <34993720+georgeschultz1998@users.noreply.github.com>
2022-12-02 23:31:09 -0500

    Finishedproduct displauy

--- myscripts.js
+++ myscripts.js
@@ -1,22 +1,54 @@
-alert("Hello!");
+function updateApt5() {
+    document.getElementById("desc").innerText="Upstairs Studio Apartment";
+    document.getElementById("rent").innerText="Rent-$850";
+    document.getElementById("bed").innerText="Studio";
+    document.getElementById("bath").innerText="1 Bathroom";
+    document.getElementById("sqft").innerText="600 sqft";
+    document.getElementById("pet").innerText="Under 25 pound pet OK";
+    document.getElementById("avail").innerText="RENTED 09/22";
+    document.getElementById("apartmentimg").src = "images/Apartment5.png";
+    }
 
-APT5.addEventListener('click', function(){
-    alert("button clicked");
-});
+function updateApt6() {
+    document.getElementById("desc").innerText="Upstairs 1 Bed Apartment";
+    document.getElementById("rent").innerText="Rent-$950";
+    document.getElementById("bed").innerText="1 Bedroom";
+    document.getElementById("bath").innerText="1 Bathroom";
+    document.getElementById("sqft").innerText="700 sqft";
+    document.getElementById("pet").innerText="Under 25 pound pet OK";
+    document.getElementById("avail").innerText="AVAILABLE NOW";
+    document.getElementById("apartmentimg").src = "images/Apartment6.png";
+}
 
-APT6.addEventListener('click', function(){
-    alert("button clicked");
-});
+function updateApt8() {
+    document.getElementById("desc").innerText="Downstairs 1 Bed Apartment";
+    document.getElementById("rent").innerText="Rent-$950";
+    document.getElementById("bed").innerText="1 Bedroom";
+    document.getElementById("bath").innerText="1 Bathroom";
+    document.getElementById("sqft").innerText="700 sqft";
+    document.getElementById("pet").innerText="Under 25 pound pet OK";
+    document.getElementById("avail").innerText="AVAILABLE 01/23";
+    document.getElementById("apartmentimg").src = "images/Apartment8.png";
+}
 
-APT8.addEventListener('click', function(){
-    alert("button clicked");
-});
+function updateApt14() {
+    document.getElementById("desc").innerText="Downstairs 2 Bed Apartment";
+    document.getElementById("rent").innerText="Rent-$1,050";
+    document.getElementById("bed").innerText="2 Bedroom";
+    document.getElementById("bath").innerText="1 Bathroom";
+    document.getElementById("sqft").innerText="800 sqft";
+    document.getElementById("pet").innerText="Under 25 pound pet OK";
+    document.getElementById("avail").innerText="RENTED 11/22";
+    document.getElementById("apartmentimg").src = "images/Apartment14.png";
+}
 
-APT14.addEventListener('click', function(){
-    alert("button clicked");
-});
-
-APT15.addEventListener('click', function(){
-    alert("button clicked");
-});
-
+function updateApt15() {
+    document.getElementById("desc").innerText="Upstairs 1 Bed Apartment";
+    document.getElementById("rent").innerText="Rent-$950";
+    document.getElementById("bed").innerText="1 Bedroom";
+    document.getElementById("bath").innerText="1 Bathroom";
+    document.getElementById("sqft").innerText="700 sqft";
+    document.getElementById("pet").innerText="Under 25 pound pet OK";
+    document.getElementById("avail").innerText="RENTED 05/22";
+    document.getElementById("apartmentimg").src = "images/Apartment15.png";
+}
