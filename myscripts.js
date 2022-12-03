22aa6c4ffd80baf4918a6c3f34f59826b6cfa4ed
Anthony Schultz <34993720+georgeschultz1998@users.noreply.github.com>
2022-12-02 22:07:01 -0500

    Renaming file

---
+++ myscripts.js
@@ -0,0 +1,22 @@
+alert("Hello!");
+
+APT5.addEventListener('click', function(){
+    alert("button clicked");
+});
+
+APT6.addEventListener('click', function(){
+    alert("button clicked");
+});
+
+APT8.addEventListener('click', function(){
+    alert("button clicked");
+});
+
+APT14.addEventListener('click', function(){
+    alert("button clicked");
+});
+
+APT15.addEventListener('click', function(){
+    alert("button clicked");
+});
+
